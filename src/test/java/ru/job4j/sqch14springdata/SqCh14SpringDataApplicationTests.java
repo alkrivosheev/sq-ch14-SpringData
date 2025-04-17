7887ce33cbaa5a4b2a9370076e2a118e0da9c9f3
alkrivosheev <alkrivosheev@gmail.com>
2025-04-17 10:18:28 +0300

    init

---
+++ src/test/java/ru/job4j/sqch14springdata/SqCh14SpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.job4j.sqch14springdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqCh14SpringDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
